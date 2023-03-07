3491c63cf3fc9d42d9359b93dd8e2f38025f7946
hakan pesener <pesener@windowslive.com>
2023-03-07 20:33:22 +0300

    design modal

--- src/components/AdresModal.tsx
+++ src/components/AdresModal.tsx
@@ -22,20 +22,22 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
   return (
     <div className="fixed inset-0  bg-black bg-opacity-40 flex justify-center items-center ">
       {!isOn ? (
-        <div className="w-[600px] bg-white rounded p-8 flex flex-col justify-center items-center">
-          <div className="bg-white p-6 rounded font-bold  flex-col grid grid-cols-2">
-            <h1 className="text-xl ml-10 font-bold w-96 justify-center text-center">
+        <div className="w-[600px] h-[600px] align-top bg-white rounded p-8 flex flex-col justify-center items-center">
+          <div className="bg-white mb-8 w-full p-0 font-bold justify-between mt-0  flex ">
+            <div className="flex justify-between align-top">
+            <h1 className="text-2xl ml-16 font-bold w-96 justify-center flex items-center text-center">
               Teslimat Yöntemini Belirle{" "}
             </h1>
-            <span className="w-12 h-12 place-self-end ">
+            </div><div>
+            <span onClick={handleClick} className="w-16 h-16 place-self-end rounded-full justify-center  items-center flex hover:bg-gray-100 cursor-pointer">
               <svg
-                onClick={handleClick}
+                
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                className="w-8 h-8 text-black text-xl hover:shadow-lg shadow-slate-600 hover:bg-gray-100 items-center justify-center 	rounded-full place-self-end  cursor-pointer"
+                className="w-8 h-8 text-black text-xl  cursor-pointer"
               >
                 <path
                   stroke-linecap="round"
@@ -44,6 +46,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
                 />
               </svg>
             </span>
+            </div>
           </div>
 <div className="flex flex-col w-96 mb-4">
           <div className="bg-white  rounded font-bold ">
@@ -57,7 +60,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
           <div className="flex-col flex justify-center items-center">
           <div
             onClick={handleClick2}
-            className="bg-white  w-96  p-6 rounded mb-4 border flex font-bold cursor-pointer"
+            className="bg-white  w-96  p-5 rounded mb-4 border flex shadow-lg font-bold cursor-pointer"
           >
             <div className="w-10 h-10 ml-12 ">
               {" "}
@@ -74,7 +77,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
             </div>
           </div>
 
-          <div className="bg-white p-6 mb-4 w-96 border rounded flex font-bold cursor-pointer">
+          <div className="bg-white p-5 mb-4 w-96 shadow-lg  border rounded flex font-bold cursor-pointer">
             <img
               className="w-10 h-10 ml-12"
               src="https://www.migros.com.tr/assets/images/delivery/store.svg"
@@ -90,7 +93,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
               </div>
             </div>
           </div>
-          <div className="bg-white p-6 w-96 rounded border flex font-bold  cursor-pointer">
+          <div className="bg-white p-5 w-96 rounded border shadow-lg  flex font-bold  cursor-pointer">
          <div className="">   <img className="w-10 h-10 ml-12"
               src="https://www.migros.com.tr/assets/images/delivery/foundation.svg"
               alt="home"
