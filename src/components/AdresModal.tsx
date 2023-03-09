0e8a6fed54b6490cebd41fd1a3e094edaeb9340e
hakan pesener <pesener@windowslive.com>
2023-03-09 20:50:07 +0300

    city from design

--- src/components/AdresModal.tsx
+++ src/components/AdresModal.tsx
@@ -22,14 +22,14 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
   return (
     <div className="fixed inset-0  bg-black bg-opacity-40 flex justify-center items-center ">
       {!isOn ? (
-        <div className="w-[600px] h-[600px] align-top bg-white rounded p-8 flex flex-col justify-center items-center">
+        <div className="w-[600px] mt-0 align-top bg-white rounded p-8 flex flex-col justify-center items-center">
           <div className="bg-white mb-8 w-full p-0 font-bold justify-between mt-0  flex ">
             <div className="flex justify-between align-top">
             <h1 className="text-2xl ml-16 font-bold w-96 justify-center flex items-center text-center">
               Teslimat Yöntemini Belirle{" "}
             </h1>
             </div><div>
-            <span onClick={handleClick} className="w-16 h-16 place-self-end rounded-full justify-center  items-center flex hover:bg-gray-100 cursor-pointer">
+            <span onClick={handleClick} className="w-16 h-16 ml-12  place-self-end rounded-full justify-center  items-center flex hover:bg-gray-100 cursor-pointer">
               <svg
                 
                 xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +51,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
 <div className="flex flex-col w-96 mb-4">
           <div className="bg-white  rounded font-bold ">
             <h1>Merhaba,</h1>
-            <h1 className="text-3xl font-bold ">
+            <h1 className="text-2xl font- font-bold ">
               Siparişini nasıl getirelim?{" "}
             </h1>
             </div>
@@ -60,7 +60,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
           <div className="flex-col flex justify-center items-center">
           <div
             onClick={handleClick2}
-            className="bg-white  w-96  p-5 rounded mb-4 border flex shadow-lg font-bold cursor-pointer"
+            className="bg-white  w-96   p-5 rounded-lg mb-4 border-2 flex shadow-lg font-bold cursor-pointer"
           >
             <div className="w-10 h-10 ml-12 ">
               {" "}
@@ -77,7 +77,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
             </div>
           </div>
 
-          <div className="bg-white p-5 mb-4 w-96 shadow-lg  border rounded flex font-bold cursor-pointer">
+          <div className="bg-white p-5 mb-4 w-96 shadow-lg  border-2 rounded-lg flex font-bold cursor-pointer">
             <img
               className="w-10 h-10 ml-12"
               src="https://www.migros.com.tr/assets/images/delivery/store.svg"
@@ -93,7 +93,7 @@ const AdresModal = ({ isActive, setIsActive }: Props) => {
               </div>
             </div>
           </div>
-          <div className="bg-white p-5 w-96 rounded border shadow-lg  flex font-bold  cursor-pointer">
+          <div className="bg-white p-5 mb-12 w-96 rounded-lg border-2 shadow-lg  flex font-bold  cursor-pointer">
          <div className="">   <img className="w-10 h-10 ml-12"
               src="https://www.migros.com.tr/assets/images/delivery/foundation.svg"
               alt="home"
--- src/components/ChoosePlace.tsx
+++ src/components/ChoosePlace.tsx
@@ -1,67 +1,109 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { CityNames } from "../Json/il";
+import { Districts } from "../Json/ilce";
+import { Neighbourhoods } from "../Json/mahalle";
+import { BiChevronDown } from "react-icons/bi";
+import {SlMagnifier} from "react-icons/sl"
 type Props = {
   isOn: boolean;
   setIsOn: (active: boolean) => void;
   isActive: boolean;
   setIsActive: (active: boolean) => void;
+ 
 };
+
 const ChoosePlace = ({ isOn, setIsOn, setIsActive, isActive }: Props) => {
+  
   const handleClick = () => {
     setIsActive(false);
     console.log(isActive);
   };
 
+  const [city, setCity] = useState<string>();
+
+  const [selectedCity, setSelectedCity] =useState<string>()
+
+  const [open, setOpen] = useState<boolean>(false)
+
+  const cityNa = CityNames;
+
+  const distNa = Districts;
+
+  const neigNa = Neighbourhoods;
+
   return (
     <div>
-      <div className="w-[600px]  flex flex-col">
-        <div className="bg-white p-8 rounded font-bold flex flex-col ">
-          <svg
-            onClick={handleClick}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-black text-xl w-8 place-self-end cursor-pointer"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+      <div className="fixed inset-0  flex justify-center items-center ">
+        <div className="w-[600px] h-[600px] mt-0 align-top bg-white rounded p-8 flex flex-col justify-center items-center">
+          <div className="bg-white mb-8 w-full p-0 font-bold justify-between mt-0  flex ">
+            <div className="flex flex-col justify-between align-top self-start">
+              <h1 className="text-2xl ml-16 font-bold w-96 justify-center flex items-center text-center">
+                Adresime Gelsin
+              </h1>
+              <h1 className="text-xl mt-0 ml-16 justify-center text-center font-bold ">
+                Siparişini nereye getirelim?
+              </h1>
+            </div>
+            <div className="mt-0 mb-4">
+              <span
+                onClick={handleClick}
+                className="w-16 ml-12  h-16 place-self-end rounded-full justify-center  items-center flex hover:bg-gray-100 cursor-pointer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-8 h-8 text-black text-xl  cursor-pointer"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col mt-0 w-96 mb-4">
+            <div className="bg-white justify-center mt-0 text-center rounded font-bold "></div>
+          </div>
+          <div className="flex-col flex justify-center items-center">
+            <div onClick={()=>setOpen(!open)} className="bg-white  w-96   p-1 rounded-lg mb-4 border-2 flex justify-between items-center shadow-lg font-bold cursor-pointer">
+              {selectedCity? selectedCity : "il"}
+              <BiChevronDown size={30} />
+            </div>
+            <ul className={`bg-white  overflow-y-auto w-96 border-1 shadow-lg  ${open ? "max-h-60" : "max-h-0"}`}>
+              <div  className="flex justify-center items-center  px-2 sticky top-0 bg-white">
+              <SlMagnifier size={27} className="text-gray-400 bg-white border-l-2 border-t-2 border-b-2  rounded-l-lg" />
+                <input className="border-t-2 border-b-2 border-r-2  w-96 rounded-r-lg outline-none " type="text" value={city} onChange={(e)=>setCity(e.target.value.toLowerCase())}>
+                
+                </input>
+              </div>
+              
+              {cityNa.map((cit: any) => (
+                <li
+                  className={`p-2  text-sm cursor-pointer hover:text-orange-400 active:bg-gray-200 ${cit.toLowerCase()}
+                  ${cit.toLowerCase() === selectedCity?.toLowerCase() && 'text-orange-400'} ${cit.toLowerCase().startsWith(city)? "block" : "hidden" }`}
+                  key={cit.index}
+                  onClick={()=>{
+                    if(cit.toLowerCase() !== selectedCity?.toLowerCase()){ 
+                    setSelectedCity(cit)
+                    }
+                  }}
+                >
+                  {cit}
+                </li>
+              ))}
+            </ul>
 
-          <h1 className="text-3xl font-bold justify-center text-center">
-            Telimat Yöntemini Belirle{" "}
-          </h1>
-        </div>
+        
+            
 
-        <div className="bg-white p-8 rounded font-bold ">
-          Merhaba,
-          <h1 className="text-3xl font-bold ">Siparişini nasıl getirelim? </h1>
-        </div>
-        <div className="bg-white p-8 rounded border-4 cursor-pointer">
-          <img
-            src="https://www.migros.com.tr/assets/images/delivery/address.svg"
-            alt="home"
-          />
-          Adresime Gelsin
-        </div>
 
-        <div className="bg-white p-8 rounded border-4 cursor-pointer">
-          <img
-            src="https://www.migros.com.tr/assets/images/delivery/store.svg"
-            alt="home"
-          />
-          Tıkla Gel Al{" "}
-        </div>
-        <div className="bg-white p-8 rounded border-4 cursor-pointer">
-          <img
-            src="https://www.migros.com.tr/assets/images/delivery/foundation.svg"
-            alt="home"
-          />
-          Bağış Yapacağım mı acaba?
+      
+          </div>
         </div>
       </div>
     </div>
